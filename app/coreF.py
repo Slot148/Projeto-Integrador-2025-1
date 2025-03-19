9fb85cb649d9c02482736edc8cfb4b03629bb7ce
Nicolas Anderson <141173875+Slot148@users.noreply.github.com>
2025-03-19 15:08:45 -0300

    <feat>(US23) Processamento de arquivos
    Agora é possível processar documentos enviados junto do nome e ra

--- app/coreF.py
+++ app/coreF.py
@@ -5,9 +5,10 @@ from datetime import datetime
 
 #Caaminho do JSON
 dataFile = 'data/atestado.json'
+fileFolder = "atestados"
 
 #Para definir o dia do upload(alterar futurramente)
-diaAtual = datetime.today().strftime('%Y/%m/%d')
+diaAtual = datetime.today().strftime('%Y-%m-%d')
 
 #Função para leitura do JSON
 def readJSON():
@@ -21,8 +22,10 @@ def readJSON():
 
 #Função para escrever no JSON
 def writeJSON(data):
+    os.makedirs(os.path.dirname(dataFile), exist_ok=True)  
     with open(dataFile, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
+
 
 #Função que recebe os valores dos campos do formulario
 ##Ela deve ser chamada imediatamente como resposta ao submit do formulario (especificamente o de atestados)
@@ -30,14 +33,26 @@ def processingJSON():
     nomeAluno = request.form.get('nomeAluno') #O campo input deve haver a string requisitada como id
     raAluno = request.form.get('raAluno') #O campo input deve haver a string requisitada como id
     dataUpload = diaAtual
+    newFile = request.files.get('insertFile')
     
-    if not nomeAluno or not raAluno:
+    if not nomeAluno or not raAluno or not newFile:
         return "preencha todos os campos"
     if not raAluno.isdigit():
        return "todos os caracteres devem ser numéricos"
+
+    #verificando se as pastas existem
+    os.makedirs(fileFolder, exist_ok=True)
     
+    #Salva o caminho do arquivo
+    fileName =f"{nomeAluno}_{diaAtual}.pdf"
+    filePath = os.path.join(fileFolder, fileName)
+
+    #salva o arquivo no diretorio atestados
+    newFile.save(filePath)
+
+    #adiciona uma nova sessão ao JSON
     data = readJSON()
-    novaInserção = ({"Nome": nomeAluno, "R.A": int(raAluno), "Data de Upload": dataUpload})
+    novaInserção = ({"Nome": nomeAluno, "R.A": int(raAluno), "Data de Upload": dataUpload, "Atestado": filePath})
     data.append(novaInserção)
     writeJSON(data)
     
@@ -47,4 +62,13 @@ def processingJSON():
 #função para consulta simples dos dados em json
 def dataConsult():
     data = readJSON()
-    return jsonify (data)
+    return jsonify(data)
+
+###chame-o da seguinte forma para debugar###
+# @app.route("/consult", methods=['GET'])
+# def consult():
+#     data = readJSON()
+#     return app.response_class(
+#         response=json.dumps(data, ensure_ascii=False, indent=4),
+#         mimetype='application/json'
+#     )
