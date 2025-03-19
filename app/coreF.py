75104406955b570a50698213032019c84fb8e899
Nicolas Anderson <141173875+Slot148@users.noreply.github.com>
2025-03-19 15:37:15 -0300

    <feat>(US23) processamento de dados Scrum

--- app/coreF.py
+++ app/coreF.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 #Caaminho do JSON
 dataFile = 'data/atestado.json'
+scrumFile = 'data/scrum-team.json'
 fileFolder = "atestados"
 
 #Para definir o dia do upload(alterar futurramente)
@@ -28,8 +29,32 @@ def writeJSON(data):
 
 
 #Função que recebe os valores dos campos do formulario
+##Ela deve ser chamada imediatamente como resposta ao submit do formulario (especificamente o de scrum)
+def processingScrum():
+    nomeAlunoScrum = request.form.get("nomeAlunoScrum")
+    nomeEquipeScrum = request.form.get("nomeEquipeScrum")
+    teamFunction = request.form.get("teamFunction")
+
+    # Pegando as notas do formulário
+    try:
+        nota1 = int(request.form.get("nota1"), 0)
+        nota2 = int(request.form.get("nota2"), 0)
+        nota3 = int(request.form.get("nota3"), 0)
+        nota4 = int(request.form.get("nota4"), 0)
+    except ValueError:
+        return "Notas devem ser números válidos"
+
+    if not nomeAlunoScrum or not nomeEquipeScrum or not teamFunction or not nota1 or not nota2 or not nota3 or not nota4:
+        return "Preencha todos os campos corretamente"
+    
+    data = readJSON()
+    novaInserção = ({"Nome": nomeAlunoScrum, "Equipe": nomeEquipeScrum, "Função": teamFunction, "Notas": [nota1, nota2, nota3, nota4]})
+    data.append(novaInserção)
+    writeJSON(data)
+
+#Função que recebe os valores dos campos do formulario
 ##Ela deve ser chamada imediatamente como resposta ao submit do formulario (especificamente o de atestados)
-def processingJSON():
+def processingAtestados():
     nomeAluno = request.form.get('nomeAluno') #O campo input deve haver a string requisitada como id
     raAluno = request.form.get('raAluno') #O campo input deve haver a string requisitada como id
     dataUpload = diaAtual
