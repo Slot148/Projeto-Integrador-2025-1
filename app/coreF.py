0fc585cf9b5bb1dc772859c17071b49ba1155bcb
Nicolas Anderson <141173875+Slot148@users.noreply.github.com>
2025-03-24 18:47:54 -0300

    <feat>(US18)Estrutura basica para pagina de upload

--- app/coreF.py
+++ app/coreF.py
@@ -12,88 +12,86 @@ fileFolder = "atestados"
 diaAtual = datetime.today().strftime('%Y-%m-%d')
 
 #Função para leitura do JSON
-def readJSON():
-    if os.path.exists(dataFile) :
-        with open(dataFile, "r", encoding="utf-8") as f:
+def readJSON(files):
+    if os.path.exists(files) :
+        with open(files, "r", encoding="utf-8") as f:
             try:
                  return json.load(f)
             except json.JSONDecodeError:
                 return []
     return []
 
+
 #Função para escrever no JSON
-def writeJSON(data):
-    os.makedirs(os.path.dirname(dataFile), exist_ok=True)  
-    with open(dataFile, "w", encoding="utf-8") as f:
+def writeJSON(data, files):
+    os.makedirs(os.path.dirname(files), exist_ok=True)  
+    with open(files, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
 
 #Função que recebe os valores dos campos do formulario
 ##Ela deve ser chamada imediatamente como resposta ao submit do formulario (especificamente o de scrum)
 def processingScrum():
-    nomeAlunoScrum = request.form.get("nomeAlunoScrum")
-    nomeEquipeScrum = request.form.get("nomeEquipeScrum")
-    teamFunction = request.form.get("teamFunction")
-
-    # Pegando as notas do formulário
     try:
-        nota1 = int(request.form.get("nota1"), 0)
-        nota2 = int(request.form.get("nota2"), 0)
-        nota3 = int(request.form.get("nota3"), 0)
-        nota4 = int(request.form.get("nota4"), 0)
-    except ValueError:
-        return "Notas devem ser números válidos"
+        nomeAlunoScrum = request.form['nomeAlunoScrum']
+        nomeEquipeScrum = request.form['nomeEquipeScrum']
+        teamFunction = request.form['teamFunction']
 
-    if not nomeAlunoScrum or not nomeEquipeScrum or not teamFunction or not nota1 or not nota2 or not nota3 or not nota4:
+        # Pegando as notas do formulário
+        try:
+            nota1 = int(request.form['nota1'])
+            nota2 = int(request.form['nota2'])
+            nota3 = int(request.form['nota3'])
+            nota4 = int(request.form['nota4'])
+            
+        except ValueError:
+            return "Notas devem ser números válidos"
+
+    except:
         return "Preencha todos os campos corretamente"
     
-    data = readJSON()
+    nomeAlunoScrum = nomeAlunoScrum.lower()
+
+    data = readJSON(scrumFile)
     novaInserção = ({"Nome": nomeAlunoScrum, "Equipe": nomeEquipeScrum, "Função": teamFunction, "Notas": [nota1, nota2, nota3, nota4]})
     data.append(novaInserção)
-    writeJSON(data)
+    writeJSON(data, scrumFile)
 
 #Função que recebe os valores dos campos do formulario
 ##Ela deve ser chamada imediatamente como resposta ao submit do formulario (especificamente o de atestados)
 def processingAtestados():
-    nomeAluno = request.form.get('nomeAluno') #O campo input deve haver a string requisitada como id
-    raAluno = request.form.get('raAluno') #O campo input deve haver a string requisitada como id
-    dataUpload = diaAtual
-    newFile = request.files.get('insertFile')
-    
-    if not nomeAluno or not raAluno or not newFile:
+    try:
+        nomeAluno = request.form['nomeAluno'] #O campo input deve haver a string requisitada como name
+        raAluno = request.form['raAluno'] #O campo input deve haver a string requisitada como name
+        dataUpload = diaAtual
+        dataInicio = request.form['initDate']
+        dataFim = request.form['endDate']
+        newFile = request.files['insertFile']
+
+    except:
         return "preencha todos os campos"
+    
     if not raAluno.isdigit():
        return "todos os caracteres devem ser numéricos"
+
+    #padronizando os nomes
+    nomeAluno = nomeAluno.lower()
+    periodoValido = f"{dataInicio}_a_{dataFim}"
 
     #verificando se as pastas existem
     os.makedirs(fileFolder, exist_ok=True)
     
     #Salva o caminho do arquivo
-    fileName =f"{nomeAluno}_{diaAtual}.pdf"
+    fileName =f"{nomeAluno.replace(' ', '_')}_{diaAtual}.pdf"
     filePath = os.path.join(fileFolder, fileName)
 
     #salva o arquivo no diretorio atestados
     newFile.save(filePath)
 
     #adiciona uma nova sessão ao JSON
-    data = readJSON()
-    novaInserção = ({"Nome": nomeAluno, "R.A": int(raAluno), "Data de Upload": dataUpload, "Atestado": filePath})
+    data = readJSON(dataFile)
+    novaInserção = ({"Nome": nomeAluno, "R.A": int(raAluno), "Periodo de Validade": periodoValido, "Atestado": filePath, "Data de Upload": dataUpload})
     data.append(novaInserção)
-    writeJSON(data)
+    writeJSON(data, dataFile)
     
     return "Atestado enviado"         
-
-
-#função para consulta simples dos dados em json
-def dataConsult():
-    data = readJSON()
-    return jsonify(data)
-
-###chame-o da seguinte forma para debugar###
-# @app.route("/consult", methods=['GET'])
-# def consult():
-#     data = readJSON()
-#     return app.response_class(
-#         response=json.dumps(data, ensure_ascii=False, indent=4),
-#         mimetype='application/json'
-#     )
